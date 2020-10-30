a2cd778920c6b2500109bf5eaa858ddad8d7884b
Ly Nguyen <lyntc.elec@gmail.com>
2020-10-30 10:34:25 +0700

    - Init sample project Vue Apollo GraphQL

---
+++ README.md
@@ -0,0 +1,32 @@
+# vue-apollo
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
+
+### Lints and fixes files
+
+```
+yarn lint
+```
+
+### Hasura GraphQL server
+
+Before setup Vue project, you need to setup Hasura GraphQL server, like below infomations
+
+<img src="images/1.png" height="300em" />
+<img src="images/2.png" height="300em" />
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ApolloQuery :query="require('../graphql/HelloWorld.gql')">
+      <template slot-scope="{ result: { loading, error, data } }">
+        <div v-if="loading" class="loading apollo">Loading...</div>
+        <div v-else-if="error" class="error apollo">An error occured</div>
+        <div v-else-if="data" class="result apollo">{{ data }}</div>
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
